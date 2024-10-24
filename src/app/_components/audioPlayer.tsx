309f22aca4f4c628b8c6dd6d4ca7fa4bba0de9c2
Dusan <dusanbojanic1@gmail.com>
2024-10-24 19:13:36 +0200

    audioplayer to button

--- src/app/_components/audioPlayer.tsx
+++ src/app/_components/audioPlayer.tsx
@@ -3,7 +3,18 @@ import { useEffect, useRef } from "react";
 const AudioPlayer = () => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
-  useEffect(() => {
+  //   useEffect(() => {
+  //     const audio = audioRef.current;
+
+  //     if (audio) {
+  //       // Play the audio when the component mounts
+  //       audio.play().catch((error) => {
+  //         console.error("Error playing audio: ", error);
+  //       });
+  //     }
+  //   }, []);
+
+  function playAudio() {
     const audio = audioRef.current;
 
     if (audio) {
@@ -12,11 +23,12 @@ const AudioPlayer = () => {
         console.error("Error playing audio: ", error);
       });
     }
-  }, []);
+  }
 
   return (
     <div>
       {/* Hidden audio element and loop*/}
+      <button onClick={playAudio}>Play audio</button>
       <audio ref={audioRef} src="/cp2077RP.mp3" loop />
     </div>
   );
