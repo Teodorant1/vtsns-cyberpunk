c82f485eeeb0b84b5aedc720980cdd588aefe0ae
Dusan <dusanbojanic1@gmail.com>
2024-10-27 19:25:18 +0100

    package.json nonsense

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -15,7 +15,7 @@ import {
 import { type AdapterAccount } from "next-auth/adapters";
 
 /**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * This is an example of how to  use the multi-project schema feature of Drizzle ORM. Use the same
  * database instance for multiple projects.
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
