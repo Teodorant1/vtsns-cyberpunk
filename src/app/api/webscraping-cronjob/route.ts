124e3d73383453403bb05879a6029ab6f6ea86da
Dusan <dusanbojanic1@gmail.com>
2024-10-27 16:37:24 +0100

    fixed cronjob endpoint

--- src/app/api/webscraping-cronjob/route.ts
+++ src/app/api/webscraping-cronjob/route.ts
@@ -1,6 +1,5 @@
 // app/api/data/route.ts
 import { NextResponse } from "next/server";
-import { type RequestData } from "~/project-types";
 import {
   shouldRunJob,
   scrape_vtsns_CRONJOB,
@@ -9,16 +8,15 @@ import {
 // Define the expected structure of the request data
 
 export async function POST(request: Request) {
+  console.log("attempting cron job");
   try {
     // Parse the JSON body and assert the type
-    const data = (await request.json()) as RequestData;
 
     const should_run_cron = await shouldRunJob();
     if (should_run_cron === true) {
       await scrape_vtsns_CRONJOB();
     }
     // Process the data (e.g., save it to a database or perform other operations)
-    console.log("Received data:", data);
     // Respond with a success message and the received data
     return NextResponse.json({ received: true, status: 200 });
   } catch (error) {
