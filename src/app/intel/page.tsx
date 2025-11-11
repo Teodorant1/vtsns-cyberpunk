c8d71351866fe59bf328d464919661a6f5b77715
Dusan <dusanbojanic1@gmail.com>
2025-11-11 12:50:36 +0100

    added conditional rendering of links in intel

--- src/app/intel/page.tsx
+++ src/app/intel/page.tsx
@@ -15,6 +15,7 @@ import { Calendar } from "lucide-react";
 import { Calendar as CalendarComponent } from "~/components/ui/calendar";
 
 import CommentList from "../_components/intelpost/commentList";
+import Link from "next/link";
 
 export default function IntelListPage() {
   const [dateRange, setDateRange] = useState<DateRange | undefined>({
@@ -63,7 +64,7 @@ export default function IntelListPage() {
           </div>
 
           <div className="flex flex-col items-center space-x-3 md:flex-row">
-            <div>LIMIT:{limit}</div>{" "}
+            <div>LIMIT:{limit}</div>
             <Button
               onClick={() => setLimit((l) => Math.max(6, l - 6))}
               className="border-red-800 bg-gray-800 text-red-500 hover:bg-gray-700"
@@ -79,7 +80,6 @@ export default function IntelListPage() {
           </div>
         </div>
         <div className="mb-6 flex-col space-x-4">
-          {" "}
           <Popover>
             <PopoverTrigger asChild>
               <Button
@@ -92,7 +92,7 @@ export default function IntelListPage() {
                 {dateRange?.from ? (
                   dateRange.to ? (
                     <>
-                      {format(dateRange.from, "LLL dd, y")}{" "}
+                      {format(dateRange.from, "LLL dd, y")}
                       <span className="text-muted-foreground mx-2">–</span>
                       {format(dateRange.to, "LLL dd, y")}
                     </>
@@ -132,6 +132,7 @@ export default function IntelListPage() {
                     <h2 className="glitch mb-1 text-2xl text-white">
                       {i.name}
                     </h2>
+
                     {/* <div className="text-sm text-red-400">
                       {i.subject} • Difficulty {i.difficulty}
                     </div> */}
@@ -169,6 +170,14 @@ export default function IntelListPage() {
                     </Button>
                   </div>
                 </div>{" "}
+                {i.link && (
+                  <Link
+                    href={i.link}
+                    className="my-3 font-bold hover:text-red-400"
+                  >
+                    LINK
+                  </Link>
+                )}
                 {expanded[i.id] && (
                   <div className="m-2 p-2">
                     <CommentList postId={i.id} comments={i.comments} />
