01d97e28de43460c0af8a335aae7dda18630181e
Dusan <dusanbojanic1@gmail.com>
2025-11-16 15:30:06 +0100

    removed metadata from child layout

--- src/app/(requires auth)/layout.tsx
+++ src/app/(requires auth)/layout.tsx
@@ -3,12 +3,6 @@ import AccessDenied from "../access-denied/page";
 import { type Metadata } from "next";
 import { getServerAuthSession } from "~/server/auth";
 
-export const metadata: Metadata = {
-  title: "VTSNS-CYBERPUNK-NEWS",
-  description: "VTSNS-CYBERPUNK-NEWS POWERED BY T3",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
-
 export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
