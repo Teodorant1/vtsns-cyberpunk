6b1af3f47608a102a297fb2ea37f060db700eee6
Dusan <dusanbojanic1@gmail.com>
2026-07-28 19:58:19 +0200

    playing around with icons , trying to get them to work on iOS as a bookmark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,13 @@ import { getServerAuthSession } from "~/server/auth";
 export const metadata: Metadata = {
   title: "SHANDU-OBAVESHTAJNA-SLUZBHA",
   description: "SHANDU-OBAVESHTAJNA-SLUZBHA , POWERED BY T3",
-  //icons: [{ rel: "icon", url: "/favicon.ico" }],
   icons: [
     {
       rel: "icon",
+      url: "/9bc588b0-c3f9-4f55-a610-0aed7c271d14.png",
+    },
+    {
+      rel: "apple-touch-icon", // Enables iOS support
       url: "/9bc588b0-c3f9-4f55-a610-0aed7c271d14.png",
     },
   ],
